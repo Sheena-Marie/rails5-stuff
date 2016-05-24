1bb66fdeb6060ee8ed8e6d39e3945478827d2329
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-05-24 12:47:26 +1000

    added status updates and logins

--- facespace/app/controllers/home_controller.rb
+++ facespace/app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
+
   def index
+    if user_signed_in?
+      if current_user.profile
+        @profile = current_user.profile
+        @status_update = StatusUpdate.new
+        @status_updates = StatusUpdate.all.order(created_at: :desc)
+      else
+        @profile = Profile.new
+      end
+    end
   end
+
 end
